6f1cfe41c63c1fbfe6c2998b8f24c7fa154de7c2
LucasPUN <sypun321@gmail.com>
2024-01-09 10:53:24 +0800

    try

--- src/main/java/com/fsse2309/project_backend/config/EnvConfig.java
+++ src/main/java/com/fsse2309/project_backend/config/EnvConfig.java
@@ -1,6 +1,6 @@
 package com.fsse2309.project_backend.config;
 
 public class EnvConfig {
-    public static final String prodBaseUrl = "http://fsse2309-project-lucas.s3-website-ap-southeast-1.amazonaws.com";
+    public static final String prodBaseUrl = "https://www.secondlifeclothing.shop";
     public static final String devEnvBaseUrl = "http://localhost:5173";
 }
